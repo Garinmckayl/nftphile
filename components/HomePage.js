23c9dfafdcfd1cba8ea52f75cdc13b2904047b83
Natnael <natnaelgetenew@gmail.com>
2022-02-04 00:21:06 +0300

    crossorigin issue

--- components/HomePage.js
+++ components/HomePage.js
@@ -345,7 +345,7 @@ export default function HomePage() {
       <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" referrerpolicy="no-referrer"></script>
     <script src="assets/vendors/popper.js/popper.min.js"></script>
     <script src="assets/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" ></script>
     {/* <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script> */}
     <script src="assets/vendors/aos/aos.js"></script>
     <script src="assets/js/main.js"></script>
